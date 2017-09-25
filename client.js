3bcea0af6378a879ac31e480e67ae28498ea6656
Anna <anna_prih@tut.by>
2017-09-25 20:11:28 +0300

    mix questions

--- client.js
+++ client.js
@@ -2,8 +2,21 @@
  * Created by Anna on 18.09.2017.
  */
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
-
+let qaJson = JSON.parse(fs.readFileSync("qa.json"));
+let ques = qaJson.questions;
+let nowQues;
+function mixQues(ques){
+    for(i=0; i < ques.length; i++){
+        let index = Math.floor(Math.random() *(i+1));
+        let temp = ques[i];
+        ques[i] = ques[index];
+        ques[index] = temp;
+    }
+    return ques;
+};
+let questions = mixQues(ques);
 const client = new net.Socket();
 
 client.setEncoding('utf8');
