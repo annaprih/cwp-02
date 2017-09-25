f59e19c5823f4bd0f618be800a36e632852e78ab
Anna <anna_prih@tut.by>
2017-09-25 20:24:37 +0300

    start N clients

--- client-swarm.js
+++ client-swarm.js
@@ -1,3 +1,9 @@
 /**
- * Created by Anna on 18.09.2017.
+ * Created by Anna on 25.09.2017.
  */
+const N = process.argv[2];
+const {exec} = require('child_process');
+for( let i = 0; i < N; i++){
+    console.log(`Client ${i} is started`);
+    exec("node client.js");
+}
